12973f4c4635420ac32758b12bd8e5ee7b2a8114
RubiRamos <rubi.ramos2244@uppuebla.edu.mx>
2024-03-07 15:57:24 -0600

    clase HastaLuego

---
+++ src/HastaLuego.java
@@ -0,0 +1,13 @@
+public class HastaLuego {
+    public String hola;
+    public String adios;
+
+    public void setAdios(String adios) {
+        this.adios = adios;
+
+    }
+    public String getAdios(){
+        return "Adios";
+    }
+
+}
