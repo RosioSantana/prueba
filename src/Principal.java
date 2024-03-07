792c6cb0df054435618c8b6eeeab0320758f67c6
RosioSantana <rosio.santana2242@uppuebla.edu.mx>
2024-03-07 15:46:51 -0600

    1er version

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+class Hola{
+    private String saludo;
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
+    }
+}
+
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
